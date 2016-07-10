7a37a423f43dd7e1d7cc464c65150fcda2ecefb4
Bruno Barbosa <brunobt94@gmail.com>
2016-07-10 06:55:59 -0300

    ignorando eventos do node vazio

--- Alfaia/GameScene.swift
+++ Alfaia/GameScene.swift
@@ -14,7 +14,7 @@ struct PhysicsCategories{
     static let None: UInt32 = 0
     static let Note: UInt32 = 0b1 //1
     static let Circle: UInt32 = 0b10 //4
-    static let xx: UInt32 = 0b11
+    static let EmptyNote: UInt32 = 0b11
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
@@ -116,19 +116,26 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func createNote(){
         let showNote = self.notesSequence.removeFirst()
         
-        var spriteName = "bola"
-        if !showNote {
-            spriteName = "bola-cinza"
-            return
+        let spriteName = showNote ? "bola" : "bola-cinza"
+        if showNote {
+//            spriteName = "bola-cinza"
+//            return
         }
         note = SKSpriteNode(imageNamed: spriteName)
         note.xScale = 0.00003*size.width
         note.yScale = 0.00003*size.width
         note.position = CGPoint(x: size.width * 0.0, y: size.height * 0.7)
         note.physicsBody = SKPhysicsBody(circleOfRadius: note.size.width/2)
-        note.physicsBody?.categoryBitMask = PhysicsCategories.Note
+        if showNote {
+            note.physicsBody?.categoryBitMask = PhysicsCategories.Note
+            note.physicsBody?.contactTestBitMask = PhysicsCategories.Circle
+        } else {
+            note.physicsBody?.categoryBitMask = PhysicsCategories.EmptyNote
+            note.physicsBody?.contactTestBitMask = PhysicsCategories.None
+            note.name = "EmptyNote"
+        }
+        
         note.physicsBody?.collisionBitMask = PhysicsCategories.None
-        note.physicsBody?.contactTestBitMask = PhysicsCategories.Circle
         label.text = ""
         
         //Transiçao
@@ -168,6 +175,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             secondBody = contact.bodyA;
         }
         
+        if (firstBody.node?.name == "EmptyNote" || secondBody.node?.name == "EmptyNote") {
+            return
+        }
         if(firstBody.categoryBitMask == PhysicsCategories.Note && secondBody.categoryBitMask == PhysicsCategories.Circle){
                 noteDidCollideWithCircle(firstBody.node as! SKSpriteNode, circle: secondBody.node as! SKSpriteNode)
         }
@@ -187,6 +197,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             secondBody = contact.bodyA;
         }
         
+        if (firstBody.node?.name == "EmptyNote" || secondBody.node?.name == "EmptyNote") {
+            return
+        }
         if (firstBody.categoryBitMask == PhysicsCategories.Note && secondBody.categoryBitMask == PhysicsCategories.Circle) {
             noteDidCollideWithCircleEnd(firstBody.node as! SKSpriteNode, circle: secondBody.node as! SKSpriteNode)
         }
@@ -215,6 +228,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             return
         }
         let note = self.notesGenerated.removeFirst()
+        if note.name == "EmptyNote" {
+            return
+        }
         if selected{
             //Transiçao
             note.physicsBody = nil
